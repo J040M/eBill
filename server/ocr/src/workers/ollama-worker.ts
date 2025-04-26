8054f102862372dee44438e519d31e723b2e5de8
Jo Ao <jpmachacaz@gmail.com>
2025-04-26 20:33:09 +0200

    Implement zod for schema parsing. Small refactor

--- server/ocr/src/workers/ollama-worker.ts
+++ server/ocr/src/workers/ollama-worker.ts
@@ -1,34 +1,49 @@
 import { Ollama } from 'ollama'
-import { schema, Ebill } from '../schemas/schema_0.js'
+import { zodToJsonSchema } from 'zod-to-json-schema'
+import { ebillSchema } from '../schemas/schema_0.js'
+import type { Ebill, ollamaConfig } from '../types'
 
-async function ollamaWorker(config: ollamaConfig, content: string): Promise<Ebill> {
+async function ollamaWorker(config: ollamaConfig, content: string): Promise<Partial<Ebill>> {
     console.log(`Starting Ollama worker`)
     const ollama = new Ollama({ host: config.url })
-    const result = await ollama.chat({
-        model: config.model,
-        messages: [
-            { role: 'system', content: '\
-                You are a helpful assistant that receives data and structures it in the form of a receipt/bill. \
-                Validate every data to the correct field. \
-                taxes: "TAX/VAT/Steuer" (must be a number) \
-                total: "Total/Summe/Amount", (must be a number)) \
-                bill_date: "Datum/Date" (must be in date format dd-mm-yyyy) \
-                bill_due_date: "Fälligkeitsdatum"/"Due date" (must be in date format dd-mm-yyyy) \
-                ' },
-            { role: 'user', content }
-        ],
-        format: schema
-    })
+    try {
+        const result = await ollama.chat({
+            model: config.model,
+            messages: [
+                {
+                    role: 'system', content: '\
+                    The output must match this structure exactly: \
+                    {"items": { \
+                    "label": string, \
+                    "quantity": number (only numbers), \
+                    "price_unit": number (only numbers) }[] \
+                    "bill_number": string, \
+                    "issue_date": date (ISO format: YYYY-MM-DD), \
+                    "due_date": date (ISO format: YYYY-MM-DD) / if not found use the issue_date, \
+                    "supplier": string, \
+                    "tax": {"label": string, "value": number }[], \
+                    "total": number (only numbers) } \
+                    Do not include any other text or commentary. Do not explain your reasoning. \
+                    Just return a clean JSON object in the format above.' },
+                { 
+                    role: 'user', content 
+                }
+            ],
+            format: zodToJsonSchema(ebillSchema),
+        })
 
-    console.log(result.message)
+        const parsed = JSON.parse(result.message.content)
 
-    if (result.message) {
-        console.error('Error processing data:', result)
-        throw new Error("Error processing ChatMessage")
+        parsed.issue_date = new Date(parsed.issue_date)
+        parsed.due_date = new Date(parsed.due_date)
+
+        const parsedResult = ebillSchema.parse(parsed)
+
+        return parsedResult as unknown as Partial<Ebill>
+    } catch (error) {
+        console.error('Error in Ollama worker:', error)
+        throw error
     }
-
-    console.log('Terminating Ollama worker...')
-    return result.message
 }
 
 export default ollamaWorker
