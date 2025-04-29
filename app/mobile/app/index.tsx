14f66a3da854bd110ad55d0644a59d6880fe84f0
Jo Ao <jpmachacaz@gmail.com>
2025-04-29 23:33:11 +0200

    Add button to navigate the application

--- app/mobile/app/index.tsx
+++ app/mobile/app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Let's scan some documents</Text>
+      
+      <Button title="Open Camera" onPress={() => router.push("/camera")} />
+      <Button title="Open Login" onPress={() => router.push("/login")} />
+      <Button title="Open Settings" onPress={() => router.push("/settings")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: 20,
+    fontSize: 18,
+    color: "blue",
+    textDecorationLine: "underline",
+  },
+});
