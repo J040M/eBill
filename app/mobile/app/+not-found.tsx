b4c8501ecd437a86d94ebecf74b4da9472bb7214
Jo Ao <jpmachacaz@gmail.com>
2025-04-30 13:23:04 +0200

    Update 404 page

---
+++ app/mobile/app/+not-found.tsx
@@ -0,0 +1,38 @@
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops!" }} />
+      <View style={styles.container}>
+        <Text style={styles.title}>Ups, it was right here but now is not!</Text>
+        <Link href="/" style={styles.link}>
+          Let's go back home
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    backgroundColor: "#007bff",
+    color: "#fff",
+    borderRadius: 5,
+    textAlign: "center",
+  },
+});
